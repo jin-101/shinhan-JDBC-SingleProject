6a4dc32498a7ded97c110345ca8fb569fa806d14
jin-101 <120353974+jin-101@users.noreply.github.com>
2023-03-22 16:53:16 +0900

    Delete a_myProject directory

--- a_myProject/model/AccountService.java
+++
@@ -1,9 +0,0 @@
-package a_myProject.model;
-
-public class AccountService {
-	AccountDAO accDao = new AccountDAO();
-	
-	public int userLogin(String id, String pw) {
-		return accDao.userLogin(id,pw); 
-	}
-}
